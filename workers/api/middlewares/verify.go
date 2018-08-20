820267f5f592ac4038c1bac758e0a681b9dffd2c
go <go@Users-MacBook-Pro-3.local>
2018-08-20 18:18:24 +0300

    Modified custom middleware

--- workers/api/middlewares/verify.go
+++ workers/api/middlewares/verify.go
@@ -4,14 +4,18 @@ import (
 	"net/http"
 	"github.com/go-chi/chi"
 	"context"
+	"gitlab.inn4science.com/gophers/service-kit/api/render"
 )
 
 func VerifySomething() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			lang := chi.URLParam(r, "some_param")
-
-			r = r.WithContext(context.WithValue(r.Context(), "some_param", lang))
+			someParam := chi.URLParam(r, "some_param")
+			if someParam == "test" {
+				render.ResultNotFound.Render(w)
+				return
+			}
+			r = r.WithContext(context.WithValue(r.Context(), "some_param", someParam))
 			next.ServeHTTP(w, r)
 			return
 
