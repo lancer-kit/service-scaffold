e619c9bff4d07cde792d801c75dbbf2f0c24e158
go <go@Users-MacBook-Pro-3.local>
2018-08-21 18:20:15 +0300

    Fixed bug with nil pointer dereference

--- workers/api/handler/get.go
+++ workers/api/handler/get.go
@@ -7,12 +7,6 @@ import (
 )
 
 func GetValueFromMiddleware(w http.ResponseWriter, r *http.Request) {
-	type resStruct struct {
-		value interface{} `json:"value"`
-	}
-	res := new(resStruct)
 	testParam := r.Context().Value("some_param")
-	res.value = testParam
-
-	render.Success(w, res)
+	render.Success(w, testParam)
 }
