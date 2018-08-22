96b18924c1e819cdaea31fcd9fdc879c44af45b3
go <go@Users-MacBook-Pro-3.local>
2018-08-22 13:04:50 +0300

    Fixed migrations

--- dbschema/migrations/002_add_buzz_type.sql
+++ dbschema/migrations/002_add_buzz_type.sql
@@ -3,5 +3,4 @@ CREATE TYPE test_type AS ENUM ('testA', 'testB', 'testC', 'testD');
 ALTER TABLE buzz_feeds ADD COLUMN buzz_type test_type;
 
 -- +migrate Down
-ALTER TABLE buzz_feeds DROP COLUMN IF EXISTS buzz_type;
 DROP TYPE test_type;
