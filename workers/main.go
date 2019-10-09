2912de5daf66d66e4ff6d57e2228466080d29942
Eugene Babich <uuzh68@gmail.com>
2019-10-09 17:59:26 +0300

    Add init step into GetChief

--- workers/main.go
+++ workers/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/lancer-kit/service-scaffold/workers/api"
 	"github.com/lancer-kit/service-scaffold/workers/foobar"
 	"github.com/lancer-kit/uwe"
+	"github.com/lancer-kit/armory/log"
 )
 
 var WorkerChief uwe.Chief
@@ -13,6 +14,8 @@ func GetChief() *uwe.Chief {
 	WorkerChief = uwe.Chief{EnableByDefault: true}
 	WorkerChief.AddWorker(config.WorkerAPIServer, api.Server())
 	WorkerChief.AddWorker(config.WorkerFooBar, &foobar.Worker{})
+
+	WorkerChief.Init(log.Default)
 
 	return &WorkerChief
 }
